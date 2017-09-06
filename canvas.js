e593ddfb1ef3a8274869d8b7a049f8f6d188b498
Aj Tafoya <ajtafoya@outlook.com>
2017-09-05 23:35:34 -0700

    practice

--- canvas.js
+++ canvas.js
@@ -3,28 +3,96 @@ var canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var c = canvas.getContext("2d");
-var x = Math.random() * innerWidth;
-var dx = (Math.random() - 0.5) * 10;
-var dy = (Math.random() - 0.5) * 10;
-var radius  = 50;
-var y = Math.random() * innerHeight;
-function animate(){
-  c.clearRect(0,0,innerWidth,innerHeight);
-  requestAnimationFrame(animate);
-  c.beginPath();
-  c.arc(x + 1,y + 1,radius,0,2*Math.PI);
-  c.fill();
+var mouse = {
+  x:undefined,
+  y:undefined
+}
+var maxR = 40;
+var minR = 5;
+var colorArr = [
+  '#f93671',
+  '#ffc435',
+  '#95f228',
+  '#2ffbae',
+  '#f72af9'
 
-  if(x + radius > innerWidth || x - radius < 0){
-    dx = -dx;
-  }
-  if(y + radius > innerHeight || y - radius < 0){
-    dy = -dy;
-  }
+];
+window.addEventListener('mousemove', function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+  console.log(mouse);
+});
 
-  x += dx;
-  y += dy;
+function Circle(x,y,dx,dy,r){
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.r = r;
+  this.color = colorArr[Math.floor(Math.random() * colorArr.length)];
+  this.draw = function(){
+    c.beginPath();
+    c.arc(this.x,this.y,this.r,0,2 * Math.PI);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+  this.update = function(){
+    if (this.x + this.r > innerWidth || this.x - this.r < 0){
+      this.dx = -this.dx;
+    }
+    if (this.y + this.r > innerHeight || this.y - this.r < 0){
+      this.dy = -this.dy;
+    }
+    this.x+=this.dx;
+    this.y+=this.dy;
+
+
+    // interactivity
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+      if (this.r < 40) {
+        this.r += 1;
+      }
+    }
+
+
+    else if(this.r > minR){
+      this.r -= 1;
+    }
+
+
+    this.draw();
+  }
 }
 
+
+
+var circlesArr = [];
+
+
+for(var i = 0; i < 800; i++){
+  var dx = Math.random() - 0.5 * 2;
+  var dy = Math.random() - 0.5 * 2;
+  var r = Math.random() * 4 + 1;
+  var c = canvas.getContext("2d");
+  var x = Math.random() * (innerHeight - r * 2) + r;
+  var y = Math.random()  * (innerHeight - r * 2) + r;
+  circlesArr.push(new Circle(x,y,dx,dy,r))
+
+}
+console.log(circlesArr);
+
+
+
+
+
+function animate(){
+requestAnimationFrame(animate);
+c.clearRect(0,0,innerWidth, innerHeight);
+
+for (var i = 0; i < circlesArr.length; i++) {
+  circlesArr[i].update();
+}
+
+
+}
 animate();
